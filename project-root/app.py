92200b25982fc4aefbf94705ff2b0b5bb841ce78
soletevendrell <soledad.vendrell@alumnos.upm.es>
2024-11-30 12:14:01 +0100

    error en funcion duplicada

--- project-root/app.py
+++ project-root/app.py
@@ -76,9 +76,6 @@ def process_and_upload_to_sqlite(filepath):
     except Exception as e:
         print(f"Error al procesar el archivo {filepath}: {e}")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/tables')
 def list_tables():
